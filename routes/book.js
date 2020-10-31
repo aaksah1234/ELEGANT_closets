0281f9396dc517897ac9751fcc45a73e52b6076f
Aakriti Sahrawat <sahrawataakriti2@gmail.com>
2020-10-31 11:01:18 +0530

    handled ajax

--- routes/book.js
+++ routes/book.js
@@ -4,6 +4,9 @@ const conn = require('../config/connection');
 
 module.exports = meetingRouter;
 
+
+//residential meetings
+
 meetingRouter.get('/residential', (req, res, next) => {
 
     res.render('booking/book', { category: 'Residential' });
@@ -13,14 +16,20 @@ meetingRouter.get('/residential', (req, res, next) => {
 
 meetingRouter.get('/residential/getEvents', (req, res, next) => {
     var date = new Date();
-    var day;
+    var day,month;
     if (date.getDate() < 10) {
         day = `0${date.getDate()}`;
     }
     else {
         day = `${date.getDate()}`;
     }
-    var s = `${date.getFullYear()}-${date.getMonth()}-${day}`;
+    if (date.getMonth() < 10) {
+        month = `0${date.getMonth()+1}`;
+    }
+    else {
+        month = `${date.getMonth()+1}`;
+    }
+    var s = `${date.getFullYear()}-${month}-${day}`;
     conn.query('DELETE FROM meeting WHERE date<? AND category="Residential"', [s], function (err, result) {
         if (err) {
             next(err);
@@ -49,20 +58,116 @@ meetingRouter.get('/residential/getEvents', (req, res, next) => {
     });
 
 });
+
+
+// workspace meetings
+
 meetingRouter.get('/workspace', (req, res, next) => {
 
     res.render('booking/book', { category: 'Workspace' });
 
 });
-meetingRouter.get('/interior', (req, res, next) => {
-    conn.query('DELETE FROM meeting WHERE date<? AND category=?', [new Date().getDate()], function (err, result) {
+
+
+meetingRouter.get('/workspace/getEvents', (req, res, next) => {
+    var date = new Date();
+    var day,month;
+    if (date.getDate() < 10) {
+        day = `0${date.getDate()}`;
+    }
+    else {
+        day = `${date.getDate()}`;
+    }
+    if (date.getMonth() < 10) {
+        month = `0${date.getMonth()+1}`;
+    }
+    else {
+        month = `${date.getMonth()+1}`;
+    }
+    var s = `${date.getFullYear()}-${month}-${day}`;
+    conn.query('DELETE FROM meeting WHERE date<? AND category="Workspace"', [s], function (err, result) {
         if (err) {
             next(err);
         }
         else {
-            res.render('booking/book', { category: 'Interior' });
+            conn.query('SELECT * FROM meeting WHERE category="Workspace"', function (err1, result2) {
+                if (err1) {
+                    next(err1);
+                }
+                else {
+                    let output = [];
+                    result2.forEach(element => {
+                        let o = {
+                            id: element.id,
+                            title: element.category,
+                            start: element.date,
+                            end: null
+                        };
+                        output.push(o);
+                    });
+                    res.json(output);
+                }
+            });
+
         }
     });
 
 });
+
+
+
+//  interior-design meetings
+
+meetingRouter.get('/interior', (req, res, next) => {
+
+    res.render('booking/book', { category: 'Interior' });
+
+});
+
+
+meetingRouter.get('/interior/getEvents', (req, res, next) => {
+    var date = new Date();
+    var day,month;
+    if (date.getDate() < 10) {
+        day = `0${date.getDate()}`;
+    }
+    else {
+        day = `${date.getDate()}`;
+    }
+    if (date.getMonth() < 10) {
+        month = `0${date.getMonth()+1}`;
+    }
+    else {
+        month = `${date.getMonth()+1}`;
+    }
+    var s = `${date.getFullYear()}-${month}-${day}`;
+    conn.query('DELETE FROM meeting WHERE date<? AND category="Interior"', [s], function (err, result) {
+        if (err) {
+            next(err);
+        }
+        else {
+            conn.query('SELECT * FROM meeting WHERE category="Interior"', function (err1, result2) {
+                if (err1) {
+                    next(err1);
+                }
+                else {
+                    let output = [];
+                    result2.forEach(element => {
+                        let o = {
+                            id: element.id,
+                            title: element.category,
+                            start: element.date,
+                            end: null
+                        };
+                        output.push(o);
+                    });
+                    res.json(output);
+                }
+            });
+
+        }
+    });
+
+});
+
 
