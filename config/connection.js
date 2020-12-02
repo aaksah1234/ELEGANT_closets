a345bf24717abc2ae351a10d615eeabcfbb0d12d
Aakriti Sahrawat <sahrawataakriti2@gmail.com>
2020-12-02 14:01:02 +0530

    modified connection

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,41 @@
 const mysql=require('mysql');
-var conn=mysql.createConnection({
-    host:process.env.HOST,
-    user:process.env.USER,
-    password:process.env.PASS,
-    database:process.env.DATABASE
-});
 
-conn.connect((err)=>{
-    if(!err){
-        console.log('connected to database!!');
-    }
-    else{
-        console.log(err);
-    }
-});
+var conn;
+
+function startConnection() {
+    console.error('CONNECTING');
+    conn=mysql.createConnection({
+        host:process.env.HOST,
+        user:process.env.USER,
+        password:process.env.PASS,
+        database:process.env.DATABASE
+    });
+    
+    conn.connect((err)=>{
+        if (err) {
+            console.error('CONNECT FAILED', err.code);
+            startConnection();
+        }
+        else
+            console.error('CONNECTED to database');        
+    });
+    conn.on('error', function(err) {
+        if (err.fatal)
+            startConnection();
+    });
+}
+
+startConnection();
+
+// testing a select every 3 seconds :
+// to try the code you can stop mysql service => select will fail
+// if you start mysql service => connection will restart correctly => select will succeed
+setInterval(function() {
+    conn.query('select 1', function(err, results) {
+        if (err) console.log('SELECT', err.code);
+        else console.log('SELECT', results);
+    });
+}, 10000);
 
 module.exports=conn;
+
