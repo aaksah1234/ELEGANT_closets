1d39b34954d23aac954bf5746993350131b35d7c
Aakriti Sahrawat <sahrawataakriti2@gmail.com>
2020-12-02 13:12:30 +0530

    debug settings

--- migration.js
+++
@@ -1,4 +0,0 @@
-const sqlite3=require('sqlite3');
-const db=new sqlite3.Database(process.env.TEST_DATABASE||'./database.sqlite');
-
-db.run('CREATE TABLE IF NOT EXISTS Messages (id INTEGER PRIMARY KEY NOT NULL ,name TEXT NOT NULL, email TEXT NOT NULL, message TEXT NOT NULL)');
