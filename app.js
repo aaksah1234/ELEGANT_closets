dde3555264b60b17ed22c76f1883362f8884fda8
Aakriti Sahrawat <sahrawataakriti2@gmail.com>
2020-12-12 02:03:47 +0530

    removed logs

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 if(process.env.NODE_ENV !== 'production'){
     require('dotenv').config();
+ 
 }
 const express= require('express');
 const app=express();
 const bodyParser=require('body-parser');
 const errorhandler=require('errorhandler');
-const sqlite3=require('sqlite3');
-const morgan = require('morgan');
 const session=require('express-session');
 const passport=require('passport');
 const flash=require('connect-flash');
 
-const db=new sqlite3.Database(process.env.TEST_DATABASE||'./database.sqlite');
 
 const contactRouter=require('./routes/contact');
 const shopRouter=require('./routes/shop');
@@ -43,7 +41,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(bodyParser.json());
 
-app.use(morgan('dev'));
+//app.use(morgan('dev'));
 
 app.use('/user',userRouter);
 app.use('/contact',contactRouter);
--- config/connection.js
+++ config/connection.js
@@ -2,45 +2,40 @@ const mysql=require('mysql');
 
 var conn;
 
-function startConnection() {
-    console.error('CONNECTING');
-    conn=mysql.createConnection({
-        //connectionLimit : 10,
-        host:process.env.HOST,
-        user:process.env.USER,
-        password:process.env.PASS,
-        database:process.env.DATABASE
-    });
-    
-    conn.connect((err)=>{
-        if (err.fatal) {
-            console.error('CONNECT FAILED FATAL: ', err.code,err);
-            startConnection();
-        }
-        else if (err) {
-            console.error('CONNECT FAILED', err.code);
-            startConnection();
-        }
-        else
-            console.error('CONNECTED to database');        
-    });
-    conn.on('error', function(err) {
-        if (err.fatal)
-            startConnection();
-    });
-}
+conn=mysql.createConnection({
+    //connectionLimit : 10,
+    host:process.env.HOST,
+    user:process.env.USER,
+    password:process.env.PASS,
+    database:process.env.DATABASE
+});
 
-startConnection();
+conn.connect((err)=>{
+    // if (err.fatal) {
+    //     console.error('CONNECT FAILED FATAL: ', err.code,err);
+    //     //startConnection();
+    // }
+    // else 
+    if (err) {
+        console.error('CONNECT FAILED', err.code);
+        //startConnection();
+    }
+    else
+        console.error('CONNECTED to database');        
+});
+
+
+//startConnection();
 
 // testing a select every 3 seconds :
 // to try the code you can stop mysql service => select will fail
 // if you start mysql service => connection will restart correctly => select will succeed
-setInterval(function() {
-    conn.query('select 1', function(err, results) {
-        if (err) console.log('SELECT', err.code);
-        else console.log('SELECT', results);
-    });
-}, 10000);
+// setInterval(function() {
+//     conn.query('select 1', function(err, results) {
+//         if (err) console.log('SELECT', err.code);
+//         else console.log('SELECT', results);
+//     });
+// }, 10000);
 
 module.exports=conn;
 
